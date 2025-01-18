0a048b18ba4e90d9a148e8b21e969256ee14aaac
MethuParoi <paroimethu@gmail.com>
2025-01-19 00:14:31 +0600

    meal update and delete api implemented

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://share-bites-9867f.web.app"],
+    origin: ["http://localhost:5173", "https://cloud-hostel.web.app"],
     credentials: true,
   })
 );
--- routes/meal.js
+++ routes/meal.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
   const mealCollection = db.collection("mealCollection");
 
   // Add meal
-  router.post("/add-meal", async (req, res) => {
+  router.post("/add-meal", verifyToken, async (req, res) => {
     try {
       const newFood = req.body;
       const result = await mealCollection.insertOne(newFood);
@@ -56,18 +56,28 @@ module.exports = (db) => {
     }
   });
 
-  // Get single meal details
-  //   router.get("/get-meal-details/:id", async (req, res) => {
-  //     try {
-  //       const id = req.params.id;
-  //       const objectId = new ObjectId(id);
-  //       const query = { _id: objectId };
-  //       const meal = await mealCollection.findOne(query);
-  //       res.send(meal);
-  //     } catch (error) {
-  //       res.status(500).send({ error: "Failed to fetch meal details" });
-  //     }
-  //   });
+  //update a meal
+  router.patch("/update-meal/:id", verifyToken, async (req, res) => {
+    const id = req.params.id;
+    // Convert id to ObjectId
+    const objectId = new ObjectId(id);
+    const updatedMeal = req.body;
+    const result = await mealCollection.updateOne(
+      { _id: objectId },
+      { $set: updatedMeal }
+    );
+    res.send(result);
+  });
+
+  // Delete a food
+
+  router.delete("/delete-meal/:id", verifyToken, async (req, res) => {
+    const id = req.params.id;
+    // Convert id to ObjectId
+    const objectId = new ObjectId(id);
+    const result = await mealCollection.deleteOne({ _id: objectId });
+    res.send(result);
+  });
 
   return router;
 };
